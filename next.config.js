9efd08f69a6a641293acf374fbfd2903c0ab6ae1
WellingtonBT <wellington.teodoro@koper.com.br>
2022-04-16 14:51:14 -0300

    feat: add config files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withPWA = require('next-pwa')
 
-module.exports = nextConfig
+module.exports = withPWA({
+  esModule: true,
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      aggregateTimeout: 300,
+      poll: 1000
+    }
+
+    return config
+  },
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development'
+  },
+  compiler: {
+    styledComponents: true
+  }
+})
